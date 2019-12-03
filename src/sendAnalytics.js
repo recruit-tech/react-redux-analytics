149d4e0a1bf50f86ca21dce615f901204bc8417c
maxmellon <tsujiken_melon@r.recruit.co.jp>
2019-12-03 13:41:46 +0900

    refactor: eslint --fix

--- src/sendAnalytics.js
+++ src/sendAnalytics.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
-import { ReactReduxContext } from "react-redux";
-import pickBy from "lodash.pickby";
-import hoistStatics from "hoist-non-react-statics";
-import { getDisplayName, valueOrFunction, isFunction } from "./utils";
-import { sendPageView, snapshotPageProps } from "./actions";
-import { sendAnalyticsPropertyName } from "./names";
+import React, { Component } from 'react'
+import { ReactReduxContext } from 'react-redux'
+import pickBy from 'lodash.pickby'
+import hoistStatics from 'hoist-non-react-statics'
+import { getDisplayName, valueOrFunction, isFunction } from './utils'
+import { sendPageView, snapshotPageProps } from './actions'
+import { sendAnalyticsPropertyName } from './names'
 
 const composeVariables = (staticVariables, mapPropsToVariables) => (
   props,
   state
 ) => {
   if (!isFunction(mapPropsToVariables)) {
-    return { ...staticVariables };
+    return { ...staticVariables }
   }
 
-  const mappedVars = mapPropsToVariables(props, state);
-  return { ...pickBy(mappedVars, Boolean), ...staticVariables };
-};
+  const mappedVars = mapPropsToVariables(props, state)
+  return { ...pickBy(mappedVars, Boolean), ...staticVariables }
+}
 
 export default ({
   sendPageViewOnDidMount = true /* boolean | (props: Object, state: Object) => boolean */,
@@ -26,76 +26,76 @@ export default ({
   snapshotPropsOnPageView = false /* boolean | (props: Object, state: Object) => boolean */,
   mixins = [],
   ...staticVariables
-}) => WrappedComponent => {
-  const composeVars = composeVariables(staticVariables, mapPropsToVariables);
-  const shouldSendOnDidMount = valueOrFunction(sendPageViewOnDidMount);
-  const shouldSendOnDidUpdate = valueOrFunction(sendPageViewOnDidUpdate);
-  const canSendPageView = valueOrFunction(onDataReady);
-  const shouldsnapshotProps = valueOrFunction(snapshotPropsOnPageView);
+}) => (WrappedComponent) => {
+  const composeVars = composeVariables(staticVariables, mapPropsToVariables)
+  const shouldSendOnDidMount = valueOrFunction(sendPageViewOnDidMount)
+  const shouldSendOnDidUpdate = valueOrFunction(sendPageViewOnDidUpdate)
+  const canSendPageView = valueOrFunction(onDataReady)
+  const shouldsnapshotProps = valueOrFunction(snapshotPropsOnPageView)
 
   class WrapperComponent extends Component {
     constructor(props) {
-      super(props);
-      this.isPageViewScheduled = false;
-      this.preventDuplicate = false;
+      super(props)
+      this.isPageViewScheduled = false
+      this.preventDuplicate = false
     }
 
     componentDidMount() {
-      const { dispatch, getState } = this.props.ctx.store;
-      const state = getState();
+      const { dispatch, getState } = this.props.ctx.store
+      const state = getState()
       if (shouldSendOnDidMount(this.props, state)) {
-        this.schedulePageView(this.props, state, dispatch);
+        this.schedulePageView(this.props, state, dispatch)
       }
     }
 
     componentWillReceiveProps(nextProps) {
-      this.preventDuplicate = false;
-      const { dispatch, getState } = this.props.ctx.store;
-      const state = getState();
+      this.preventDuplicate = false
+      const { dispatch, getState } = this.props.ctx.store
+      const state = getState()
       if (this.isPageViewScheduled && canSendPageView(nextProps, state)) {
-        this.isPageViewScheduled = false;
-        this.preventDuplicate = true;
-        const variables = composeVars(nextProps, state);
+        this.isPageViewScheduled = false
+        this.preventDuplicate = true
+        const variables = composeVars(nextProps, state)
         if (shouldsnapshotProps(nextProps, state)) {
-          dispatch(snapshotPageProps(nextProps));
+          dispatch(snapshotPageProps(nextProps))
         }
-        dispatch(sendPageView(variables, mixins));
+        dispatch(sendPageView(variables, mixins))
       }
     }
 
     componentDidUpdate(prevProps) {
-      const { dispatch, getState } = this.props.ctx.store;
-      const state = getState();
+      const { dispatch, getState } = this.props.ctx.store
+      const state = getState()
       if (shouldSendOnDidUpdate(prevProps, this.props, state)) {
-        this.schedulePageView(this.props, state, dispatch);
+        this.schedulePageView(this.props, state, dispatch)
       }
     }
 
     schedulePageView(props, state, dispatch) {
       if (canSendPageView(props, state)) {
         if (this.preventDuplicate) {
-          return;
+          return
         }
-        const variables = composeVars(props, state);
+        const variables = composeVars(props, state)
         if (shouldsnapshotProps(props, state)) {
-          dispatch(snapshotPageProps(props));
+          dispatch(snapshotPageProps(props))
         }
-        dispatch(sendPageView(variables, mixins));
+        dispatch(sendPageView(variables, mixins))
       } else {
-        this.isPageViewScheduled = true;
+        this.isPageViewScheduled = true
       }
     }
 
     render() {
-      const { store } = this.props.ctx;
+      const { store } = this.props.ctx
       return (
         <WrappedComponent
           {...{
             ...this.props,
-            store
+            store,
           }}
         />
-      );
+      )
     }
   }
 
@@ -104,16 +104,16 @@ export default ({
       <ReactReduxContext.Consumer>
         {({ store }) => <WrapperComponent {...props} ctx={{ store }} />}
       </ReactReduxContext.Consumer>
-    );
+    )
   }
 
   WrapperComponentWithContext.displayName = `SendAnalytics(${getDisplayName(
     WrappedComponent
-  )})`;
+  )})`
 
   // let the ensurePageView HoC know that this component implments sendAnalytics
   WrapperComponentWithContext[sendAnalyticsPropertyName] =
-    WrapperComponentWithContext.displayName;
+    WrapperComponentWithContext.displayName
 
-  return hoistStatics(WrapperComponentWithContext, WrappedComponent);
-};
+  return hoistStatics(WrapperComponentWithContext, WrappedComponent)
+}
--- test/sendAnalytics/create.test.js
+++ test/sendAnalytics/create.test.js
@@ -1,60 +1,60 @@
-import "jsdom-global/register";
-import { describe, it } from "mocha";
-import { expect } from "chai";
-import React from "react";
-import { createStore } from "redux";
-import { configure, mount } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import { sendAnalyticsPropertyName } from "../../src/names";
-import sendAnalytics from "../../src/sendAnalytics";
-import { topPageProps } from "../_data/props";
-import { mockState1 } from "../_data/state";
-import MockComponent from "../_data/component";
+import 'jsdom-global/register'
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+import React from 'react'
+import { Provider, createStore } from 'redux'
+import { configure, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { sendAnalyticsPropertyName } from '../../src/names'
+import sendAnalytics from '../../src/sendAnalytics'
+import { topPageProps } from '../_data/props'
+import { mockState1 } from '../_data/state'
+import MockComponent from '../_data/component'
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
 
-describe.skip("default", () => {
-  let Component;
-  let wrapper;
+describe.skip('default', () => {
+  let Component
+  let wrapper
   beforeEach(() => {
-    const store = createStore(state => ({ ...state }), mockState1);
-    Component = sendAnalytics({})(MockComponent);
+    const store = createStore((state) => ({ ...state }), mockState1)
+    Component = sendAnalytics({})(MockComponent)
     wrapper = mount(
       <Component {...topPageProps} />,
       <Provider store={store} />
-    );
-  });
-  it("displayName", () => {
+    )
+  })
+  it('displayName', () => {
     expect(Component.displayName).to.equal(
-      "SendAnalytics(ThisIsMockComponent)"
-    );
+      'SendAnalytics(ThisIsMockComponent)'
+    )
     expect(wrapper.type().displayName).to.equal(
-      "SendAnalytics(ThisIsMockComponent)"
-    );
-  });
-  it("sendAnalytics property", () => {
+      'SendAnalytics(ThisIsMockComponent)'
+    )
+  })
+  it('sendAnalytics property', () => {
     expect(Component[sendAnalyticsPropertyName]).to.equal(
-      "SendAnalytics(ThisIsMockComponent)"
-    );
+      'SendAnalytics(ThisIsMockComponent)'
+    )
     expect(wrapper.type()[sendAnalyticsPropertyName]).to.equal(
-      "SendAnalytics(ThisIsMockComponent)"
-    );
-  });
-  it("non react property of wrapped component", () => {
-    expect(Component["non-react-property"]).to.equal(
-      "non react property of MockComponent"
-    );
-    expect(wrapper.type()["non-react-property"]).to.equal(
-      "non react property of MockComponent"
-    );
-  });
-  it("store is connected", () => {
-    const store = wrapper.context().store;
-    expect(store).to.have.property("dispatch");
-    expect(store).to.have.property("getState");
-  });
-  it("this.isPageViewScheduled is default false", () => {
-    const instance = wrapper.instance();
-    expect(instance.isPageViewScheduled).to.equal(false);
-  });
-});
+      'SendAnalytics(ThisIsMockComponent)'
+    )
+  })
+  it('non react property of wrapped component', () => {
+    expect(Component['non-react-property']).to.equal(
+      'non react property of MockComponent'
+    )
+    expect(wrapper.type()['non-react-property']).to.equal(
+      'non react property of MockComponent'
+    )
+  })
+  it('store is connected', () => {
+    const store = wrapper.context().store
+    expect(store).to.have.property('dispatch')
+    expect(store).to.have.property('getState')
+  })
+  it('this.isPageViewScheduled is default false', () => {
+    const instance = wrapper.instance()
+    expect(instance.isPageViewScheduled).to.equal(false)
+  })
+})
